bc393dddc01e9cc6ff4d0bfa20431e557d55f16a
P.Zbarazkyy <mtu09@ukr.net>
2021-10-09 19:00:40 +0300

    Configurations for Servlet

---
+++ src/main/java/tomcat/controller/IndexController.java
@@ -0,0 +1,18 @@
+package tomcat.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/* Servlet class или Controller  это таки классы которые нам помогают принимать Http запросы и отправлять Http ответы */
+public class IndexController extends HttpServlet {
+    /* наследуйтесь от калла HttpServlet и переопределяем методы */
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);  /* указываем какую именно страничку возвращаем */
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Index Page</title>
+</head>
+<body>
+<h5>Hello world!</h5>
+</body>
+</html>
+<%-- это наша страничка которую мы возвращаем из контролере, созданна она в html --%>
+<%--путь к этой страничке должен быть  src/main/webapp/WEB-INF/views/index.jsp --%>
+
